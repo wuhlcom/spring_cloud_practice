75b2a215c887ec727ff62e27eb1a2016fb2a4e5f
wuhlcom <378433855@qq.com>
2017-06-23 11:46:16 +0800

    ribbon

---
+++ README.md
@@ -0,0 +1,7 @@
+###spring cloud练习
+1.eureka server
+2.eureka client
+3.zuul
+4.ribbon
+---
+==All right==
--- zhilu-ribbon-server/src/main/java/com/zhilu/simple/ZhiluRibbonServerApplication.java
+++ zhilu-ribbon-server/src/main/java/com/zhilu/simple/ZhiluRibbonServerApplication.java
@@ -5,9 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
-@EnableDiscoveryClient
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ZhiluRibbonServerApplication {   
 	public static void main(String[] args) {
 		SpringApplication.run(ZhiluRibbonServerApplication.class, args);
